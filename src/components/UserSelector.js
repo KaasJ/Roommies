b1af63f4686a519d259921394a57b08d80f76fca
KaasJ <jorritstein@gmail.com>
2018-11-05 18:41:42 +0100

    v0.02

--- src/components/UserSelector.js
+++ src/components/UserSelector.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 
 
 export default function UserSelector() {
-    return (
-      
+    return (<div>      
 
-   
+      <h1>Hello! Welcome to <strong>ROOMMIES</strong></h1>
       
     <div className="Buttons">
      <div className="Button1"> <button> <Link to={`/lookingforaroommie/`}>Looking for a roommie!</Link> </button></div>
     <div classname="Button2"><button><Link to={`/offeringaroommie/`}>Offering a roommie!</Link></button></div>
+    </div>
     </div>
     )
   }
